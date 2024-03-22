c29fdb495b7d932978609e83c93f8db62e4e8e33
bahriilhame <bahri.ilhame@gmail.com>
2024-03-22 17:39:59 +0000

    added the connection to MongoDB

---
+++ backend/app.js
@@ -0,0 +1,17 @@
+const express=require('express')
+const DBConnection=require('./config/DBConnection')
+require('dotenv').config()
+
+// DataBase Connection
+DBConnection()
+
+// The app
+const app=express()
+
+// Middleware
+app.use(express.json())
+
+// Running server
+const PORT=process.env.PORT || 8000
+app.listen(PORT,()=>console.log('Server is running on', process.env.NODE_ENV,'mode, on port: ',process.env.PORT))
+
